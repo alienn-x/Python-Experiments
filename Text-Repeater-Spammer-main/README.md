5b20622ab5ce6fb595fda3282e25b2ba62a17d6b
alienn-x <101036190+alienn-x@users.noreply.github.com>
2022-04-02 11:04:49 +0530

    Add files via upload

---
+++ Text-Repeater-Spammer-main/README.md
@@ -0,0 +1,4 @@
+# Text-Repeater-Spammer
+Text spammer using python
+Don't hack nasa using it, i would not be responsible (obviosly you cant hack nasa using this program) <br>
+
---
+++ Text-Repeater-Spammer-main/spammer888.py
@@ -0,0 +1,40 @@
+import pyautogui as pg
+import time
+from tkinter import *
+
+# Function called by Button
+def spamm():
+    b = _input.get()
+    a = no_of_time.get()
+
+    print(b)
+    time.sleep(5)
+
+    for i in range(a):
+        pg.write(b)
+        pg.press("enter")
+
+# Basic Lines
+root = Tk()
+root.geometry("500x200")
+root.title("Py Spammer")
+
+# Making lables and packing them with grid
+lab = Label(text="Enter text you wanna spam:").grid(row=0, column=1)
+lab = Label(text="Enter no of times:").grid(row=1, column=1)
+
+# Inializing Variables
+_input = StringVar()
+no_of_time = IntVar()
+
+# Making entries
+_entry = Entry(root, textvariable=_input).grid(row=0, column=2)
+_no_times = Entry(root, textvariable=no_of_time).grid(row=1, column=2)
+
+# Making Button
+Button(text="Submit", command=spamm).grid(row=2, column=2)
+
+
+root.mainloop()
+
+# print(pg.position())
